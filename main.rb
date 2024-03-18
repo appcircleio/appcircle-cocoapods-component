2c8093985504a2c0bfd5359f07db5b3d0cb4a929
boztopuz <93190375+boztopuz@users.noreply.github.com>
2024-03-18 17:08:34 +0300

    BE-2669 Simplify CocoaPods Install(#1)
    It has simplified implementation so that, in most cases, the build pipeline will use the system-installed version of Cocoapods.
    
    The user can also modify it using the step UI in the workflow editor.

--- main.rb
+++ main.rb
@@ -1,75 +1,53 @@
+# frozen_string_literal: true
+
 require 'open3'
 require 'pathname'
 
-project_path = ENV["AC_PROJECT_PATH"] || abort('Missing project path.')
-repository_path = ENV["AC_REPOSITORY_DIR"]
+project_path = ENV['AC_PROJECT_PATH'] || abort('Missing project path.')
+repository_path = ENV['AC_REPOSITORY_DIR']
 
-cocoapods_version = (ENV["AC_COCOAPODS_VERSION"] != nil && ENV["AC_COCOAPODS_VERSION"] !="") ? ENV["AC_COCOAPODS_VERSION"] : nil
+cocoapods_version = !ENV['AC_COCOAPODS_VERSION'].nil? && ENV['AC_COCOAPODS_VERSION'] != '' ? ENV['AC_COCOAPODS_VERSION'] : nil
 
 project_dir_path = repository_path ? (Pathname.new repository_path).join(File.dirname(project_path)) : File.dirname(project_path)
-cocoapods_podfile_path = File.join(project_dir_path,"Podfile")
-cocoapods_podfilelock_path = File.join(project_dir_path,"Podfile.lock")
-# cocoapods_project_path = File.join(project_dir_path,"Pods","Pods.xcodeproj")
+cocoapods_podfile_path = File.join(project_dir_path, 'Podfile')
 
 unless File.exist?(cocoapods_podfile_path)
-    puts "Podfile does not exists."
-    exit 0
+  puts 'Podfile does not exists.'
+  exit 0
 end
 
-if File.extname(project_path) != ".xcworkspace"
-    puts "Project extension must be xcworkspace."
-    exit 0
+if File.extname(project_path) != '.xcworkspace'
+  puts 'Project extension must be xcworkspace.'
+  exit 0
 end
-
-# if File.exist?(cocoapods_project_path)
-#     puts "Pods already installed."
-#     exit 0
-# end
 
 def runCommand(command)
-    puts "@@[command] #{command}"
-    unless system(command)
-      exit $?.exitstatus
-    end
+  puts "@@[command] #{command}"
+  return if system(command)
+
+  exit $?.exitstatus
+end
+
+def podInstall(cocoapods_version, project_dir_path)
+  cocoapods_cmd_prefix = cocoapods_version ? "pod _#{cocoapods_version}_" : 'pod'
+
+  Dir.chdir(project_dir_path) do
+    runCommand("#{cocoapods_cmd_prefix} install")
+  end
 end
 
 if cocoapods_version.nil?
-  if File.exist?(cocoapods_podfilelock_path)
-    versionArray = File.read(cocoapods_podfilelock_path).scan(/(?<=COCOAPODS: )(.*)/)[0]
-    if versionArray && versionArray[0]
-      cocoapods_version = versionArray[0]
-      puts "Podfile.lock version = #{cocoapods_version}"
-      if `which rbenv`.empty?
-        runCommand("sudo gem install cocoapods -v #{cocoapods_version} --no-document")
-      else
-        runCommand("gem install cocoapods -v #{cocoapods_version} --no-document")
-      end
-    end
+  puts 'Using System Default pod version.'
+  runCommand('pod --version')
+  runCommand('pod repo update')
+  Dir.chdir(project_dir_path) do
+    runCommand('pod install')
   end
 else
   puts "Cocoapods version = #{cocoapods_version}"
-  if `which rbenv`.empty?
-    runCommand("sudo gem install cocoapods -v #{cocoapods_version} --no-document")
-  else
-    runCommand("gem install cocoapods -v #{cocoapods_version} --no-document")
-  end
-end
-
-unless cocoapods_version.nil?
+  runCommand("gem install cocoapods -v #{cocoapods_version} --no-document")
   runCommand("pod _#{cocoapods_version}_ setup")
-  runCommand("pod _#{cocoapods_version}_ repo update")
-else
-  runCommand("pod --version")
-  runCommand("pod repo update")
-end
-
-Dir.chdir(project_dir_path) do
-    command = "pod"
-    unless cocoapods_version.nil?
-      command += " _#{cocoapods_version}_"
-    end
-    command += " install"
-    runCommand(command)
+  podInstall(cocoapods_version, project_dir_path)
 end
 
 exit 0
